81f349af20b20f2376e09517ba8075409021c781
tvpeter <withtvpeter@gmail.com>
2019-10-19 12:23:06 +0100

    add user registration and login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class AuthController extends Controller
+{
+    /**
+     * Register a new user
+     * @param  - http request object
+     * @return - response object
+     */
+    public function register(Request $request){
+
+        $validated = $request->validate([
+            'name' => 'required|min:3',
+            'email' => 'required|email',
+            'password' => 'required|min:8',
+        ]);
+
+        $user = User::create($validated);
+
+        $token = auth()->login($user);
+
+        return $this->respondWithToken($token);
+
+    }
+
+    /**
+     * login a user
+     * @param - http request object
+     */
+
+     public function login(Request $request){
+         $credentials = $request->only(['email', 'password']);
+
+         if(!$token = auth()->attempt($credentials)){
+             return response()->json(['error' => 'Unauthorized'], 401);
+         }
+
+         return $this->respondWithToken($token);
+     }
+
+     /** 
+      * helps to send our response with token
+      * @param - token - the generated token
+      */
+
+      protected function respondWithToken($token){
+          return response()->json([
+              'access_token' => $token,
+              'token_type' => 'bearer',
+              'expires_in' => auth()->factory()->getTTL() * 60
+          ]);
+      }
+
+
+}
--- app/User.php
+++ app/User.php
@@ -5,8 +5,9 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -45,4 +46,22 @@ class User extends Authenticatable
      public function books(){
         return $this->hasMany(Book::class);
      }
+
+     /**
+      * implementation of JWTSubject methods
+      * return the identifier
+      */
+      public function getJWTIdentifier()
+      {
+          return $this->getKey();
+      }
+
+      /**
+       * implementation of JWTSubject methods
+       * allows adding of JWT Claims
+       */
+      public function getJWTCustomClaims()
+      {
+          return [];
+      }
 }
